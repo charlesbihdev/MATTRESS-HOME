184df32f8f61039da207b4ac8db0bed0b6f745d5
Charles Owusu Bih <charlesowusubih@gmail.com>
2024-06-27 01:58:23 +0000

    done with shop. Starting dashboard

--- client/src/app/FeaturedProducts.jsx
+++ client/src/app/FeaturedProducts.jsx
@@ -35,37 +35,37 @@ const FeaturedProducts = ({ products }) => {
                                         </p>
                                     </div>
                                 )}
-                                <div className="flex items-start justify-between mt-4 space-x-4">
-                                    <div>
-                                        <h3 className="text-xs font-bold text-gray-900 sm:text-sm md:text-base">
-                                            <Link
-                                                href={`/product/${product.id}`}
-                                                title={product.name}>
+                                <Link
+                                    href={`/product/${product.id}`}
+                                    title={product.name}>
+                                    <div className="flex items-start justify-between mt-4 space-x-4">
+                                        <div>
+                                            <h3 className="text-xs font-bold text-gray-900 sm:text-sm md:text-base">
                                                 {product.name}
-                                            </Link>
-                                        </h3>
-                                        <div className="flex items-center mt-2.5 space-x-px">
-                                            {Array.from(
-                                                { length: 5 },
-                                                (_, index) => (
-                                                    <svg
-                                                        key={index}
-                                                        className={`w-3 h-3 ${index < Math.floor(Math.random() * 5 + 4) ? 'text-yellow-400' : 'text-gray-300'} sm:w-4 sm:h-4`}
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        viewBox="0 0 20 20"
-                                                        fill="currentColor">
-                                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                                                    </svg>
-                                                ),
-                                            )}
+                                            </h3>
+                                            <div className="flex items-center mt-2.5 space-x-px">
+                                                {Array.from(
+                                                    { length: 5 },
+                                                    (_, index) => (
+                                                        <svg
+                                                            key={index}
+                                                            className={`w-3 h-3 ${index < Math.floor(Math.random() * 5 + 4) ? 'text-yellow-400' : 'text-gray-300'} sm:w-4 sm:h-4`}
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            viewBox="0 0 20 20"
+                                                            fill="currentColor">
+                                                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                                        </svg>
+                                                    ),
+                                                )}
+                                            </div>
+                                        </div>
+                                        <div className="text-right">
+                                            <p className="text-xs font-bold text-gray-900 sm:text-sm md:text-base">
+                                                GH₵ {product.prices[0].price}
+                                            </p>
                                         </div>
                                     </div>
-                                    <div className="text-right">
-                                        <p className="text-xs font-bold text-gray-900 sm:text-sm md:text-base">
-                                            GH₵ {product.prices[0].price}
-                                        </p>
-                                    </div>
-                                </div>
+                                </Link>
                             </div>
                         ))}
                     </div>
