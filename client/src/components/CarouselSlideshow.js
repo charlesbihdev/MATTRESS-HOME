5d54e7796434ce48b1a30fd7edabc4e520ecb2bc
Charles Owusu Bih <charlesowusubih@gmail.com>
2024-07-14 16:50:01 +0000

    added custom carousels

--- client/src/components/CarouselSlideshow.js
+++ client/src/components/CarouselSlideshow.js
@@ -7,16 +7,22 @@ export default function CarouselSlideshow() {
         <div className="!h-60 w-full sm:h-64 xl:h-80 2xl:h-96">
             <Carousel>
                 <img
-                    src="https://www.americanmattress.com/cdn/shop/collections/AM_-_Shopify_Spring_Into_Savings_1296x.jpg"
-                    alt="..."
+                    src="/carousel_images/slide1.jpg"
+                    alt="mattress slider 1"
+                />
+
+                <img
+                    src="/carousel_images/slide3.jpg"
+                    alt="mattress slider 3"
+                />
+
+                <img
+                    src="/carousel_images/slide2.jpg"
+                    alt="mattress slider 2"
                 />
                 <img
-                    src="https://www.americanmattress.com/cdn/shop/collections/50_off_Tempur-Pedic_Floor_Models_1296x.png"
-                    alt="..."
-                />
-                <img
-                    src="https://www.americanmattress.com/cdn/shop/collections/Scott_Living_By_Restonic_Collection_2d7638ec-5b0d-4a71-a282-e049bdc551ff_1296x.jpg"
-                    alt="..."
+                    src="/carousel_images/slide4.jpg"
+                    alt="mattress slider 4"
                 />
             </Carousel>
         </div>
