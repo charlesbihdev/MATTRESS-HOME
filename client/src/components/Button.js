8dfb8aeeeca0714e3eeecd29ca0a5569e92bd81c
Charles Owusu Bih <charlesowusubih@gmail.com>
2024-07-16 08:02:47 +0000

    api alone

--- client/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = ({ type = 'submit', className, ...props }) => (
-    <button
-        type={type}
-        className={`transition ease-in-out duration-150 ${className}`}
-        {...props}
-    />
-)
-
-export default Button
--- client/src/data/SizeData.js
+++
@@ -1,9 +0,0 @@
-const sizes = [
-    { id: 1, name: 'K/S' },
-    { id: 2, name: 'Q/S' },
-    { id: 3, name: 'L/S' },
-    { id: 4, name: 'M/S' },
-    { id: 5, name: 'S/S' },
-]
-
-export default sizes
