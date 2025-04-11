b3d2d2536aa2529963a9f355fe9c7b2286244c17
Charles Owusu Bih <charlesowusubih@gmail.com>
2025-04-11 15:58:36 +0000

    make product image large

--- client/src/app/product/[id]/Product.jsx
+++ client/src/app/product/[id]/Product.jsx
@@ -72,30 +72,16 @@ export default function Product({ product }) {
             <BackArrow />
 
             <div className="p-4 lg:max-w-7xl max-w-4xl mx-auto">
-                <div className="grid bg-white items-start grid-cols-1 lg:grid-cols-5 gap-12 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] p-6 rounded-lg">
+                <div className="grid bg-white items-start grid-cols-1 lg:grid-cols-5 gap-12 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] p-4 rounded-lg">
                     <div className=" lg:col-span-3 w-full lg:sticky top-0 text-center">
-                        <div className="px-4 py-10 rounded-lg shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative">
+                        <div className="p-2 rounded-lg shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative">
                             <Image
                                 src={activeImage}
                                 alt={product.name}
-                                className="w-3/4 rounded object-cover mx-auto"
+                                className="w-full rounded object-cover mx-auto"
                                 width={300}
                                 height={200}
                             />
-                            <button
-                                type="button"
-                                className="absolute top-4 right-4">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="20px"
-                                    fill="#ccc"
-                                    className="mr-1 hover:fill-[#333]"
-                                    viewBox="0 0 64 64">
-                                    <path
-                                        d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
-                                        data-original="#000000"></path>
-                                </svg>
-                            </button>
                         </div>
                         <div className="mt-6 flex flex-wrap justify-center gap-6 mx-auto">
                             {product.pictures.map((picture, index) => {
@@ -104,11 +90,11 @@ export default function Product({ product }) {
                                     <div
                                         onClick={() => handleImageChange(index)}
                                         key={picture.id}
-                                        className="w-24 h-20 flex items-center justify-center rounded-lg p-4 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] cursor-pointer">
+                                        className="w-24 h-20 flex items-center justify-center rounded-lg p-1 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] cursor-pointer">
                                         <Image
                                             src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${picture.image_path}`}
                                             alt={product.name}
-                                            className="w-full"
+                                            className="w-full h-full"
                                             width={100}
                                             height={50}
                                         />
